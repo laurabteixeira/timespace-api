818667a577b2f61b4731ad3a73e2212675601fa7
laurabteixeira <laurabteixeira143@gmail.com>
2023-05-19 21:51:11 -0300

    crud criado

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import fastify from 'fastify'
+import { memoriesRoutes } from './routes/memories'
+import cors from '@fastify/cors'
+
+const app = fastify()
+
+app.register(cors, {
+  origin: true, // todas as ULRs de frontend poderão acessar o backend.
+})
+
+// app.register((app) => memoriesRoutes(app))
+app.register(memoriesRoutes)
+
+// 'register()' é um método do Fastify que, nesse caso, retorna o array de usuários (ou array vazio, caso não haja nenhum).
+
+// Alguns HTTP Methods: GET (listar), POST (criar), PUT (atualizar parcialmente), PATCH (atualizar totalmente), DELETE (deletar), HEAD, UPDATE, OPTIONS... Os mais usados sao os 5 primeiros.
+
+// O app.listen serve para o nosso servidor http ouvir as requisições "get, post, delete..." dadas na URL. Ele recebe um objeto de configurações, e a mais importante das configurações é a porta.
+// O Listen devolve uma promisse. A promisse significa algo que pode demorar para acontecer.
+// O 'then()' roda um código após a promisse ser executada, sem afetar o resto do código.
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('😎 HTTP Server running on http://localhost:3333')
+    // Para inserir emojis: 'windows + .'
+  })
